1a586b20c814da86b908ea5a6adf87bc9e593345
Lei Xu <leilei@172-16-41-76.DYNAPOOL.NYU.EDU>
2018-05-09 17:07:24 -0400

    add args in bash file

--- run.sh
+++ run.sh
@@ -12,8 +12,9 @@
 
 module load pytorch/python3.6/0.3.0_4
 module load torchvision/python3.5/0.1.9
+module load scipy/intel/0.19.1
 
 module load cuda/8.0.44
 module load cudnn/8.0v5.1
 
-time python3 zi2zi.py --n_epochs 250  --sample_interval 50  --generator_type "resnet"  --train_size 100 
+time python3 zi2zi.py --n_epochs 250  --sample_interval 50  --generator_type "resnet"  --train_size 100 --checkpoint_interval 40 --augmentation 'flipleftright'
